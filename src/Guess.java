a968174f1e68383d107bf829e5e231c18f8908de
Marcin <test@gmail.com>
2019-11-29 15:33:00 +0100

    Zadanie 1

--- src/Guess.java
+++ src/Guess.java
@@ -1,5 +1,36 @@
-public class Test {
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guess {
+
     public static void main(String[] args) {
-        System.out.println("Test!!!");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+
+        System.out.println("Zgadnij liczbę z przedziału od 1 do 100:");
+
+        int guess = -1;
+        while (guess != number) {
+            guess = getNumber(scanner);
+            if (guess > number) {
+                System.out.println("Za dużo!");
+                System.out.println("Zgadnij liczbę:");
+            } else if (guess < number) {
+                System.out.println("Za mało!");
+                System.out.println("Zgadnij liczbę:");
+            } else {
+                System.out.println("Zgadłeś");
+            }
+        }
+
+    }
+
+    static int getNumber(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println("Wartość nie jest liczbą, podaj jeszcze raz:");
+        }
+        return scanner.nextInt();
     }
 }
